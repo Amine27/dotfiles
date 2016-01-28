46aea968def4fc0c170705d8ec1fb4ceed9b4324
Aziz Yemloul <azizyemloul@gmail.com>
2016-01-28 20:17:06 +0100

    Revert "Merge branch 'sidah' of github.com:IGPRO-27/dotfiles"
    This reverts commit 9b887f5d8a479b5f6a49ea0fcba46eba4a853a87, reversing
    changes made to d753f5a422f2cd11589e60db0b2ab037cf89a699.

--- emacs-custom.el
+++ emacs-custom.el
@@ -4,8 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ido-enable-flex-matching t)
- '(ido-show-dot-for-dired t)
- '(w3m-key-binding (quote info)))
+ '(ido-show-dot-for-dired t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
