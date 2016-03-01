0a11d8b00efd1b50d2c84a510e80970aae193e2e
Sidwise <sid_wise@hotmail.com>
2016-03-01 15:36:44 +0100

    Add ido show dot and w3m info key binding

--- emacs-custom.el
+++ emacs-custom.el
@@ -4,7 +4,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ido-enable-flex-matching t)
- '(ido-show-dot-for-dired t))
+ '(ido-show-dot-for-dired t)
+ '(w3m-key-binding (quote info)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
