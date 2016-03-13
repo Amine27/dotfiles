3c0e6f91801480d1edc83f8d75b657b5140bb938
Aziz Yemloul <azizyemloul@gmail.com>
2016-03-13 14:05:54 +0100

    Return follow links in org-mode buffers
    File checked out from dev branch
    git checkout dev -- emacs-custom.el

--- emacs-custom.el
+++ emacs-custom.el
@@ -5,6 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(ido-enable-flex-matching t)
  '(ido-show-dot-for-dired t)
+ '(org-return-follows-link t)
  '(w3m-key-binding (quote info)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
