7b865ff5a138d5128b80b14d776e514f03569358
Aziz Yemloul <azizyemloul@gmail.com>
2016-09-17 10:48:52 +0100

    Auto revert dired-mode when changes

--- emacs-custom.el
+++ emacs-custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(dired-auto-revert-buffer (quote dired-directory-changed-p))
  '(ido-enable-flex-matching t)
  '(ido-show-dot-for-dired t)
  '(org-babel-load-languages
