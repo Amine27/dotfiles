9a2aa559a6664d2d53210f2ed1e4951a54c7c1e1
Aziz Yemloul <azizyemloul@gmail.com>
2016-09-22 10:38:38 +0100

    Make ido stop complaining about savehist-autosave
    http://stackoverflow.com/a/22395014
    Error running timer `savehist-autosave': (wrong-type-argument listp ido-buffer-history)

--- emacs.el
+++ emacs.el
@@ -144,7 +144,8 @@
 
 (load custom-file)
 (setq backup-by-copying       t
-      backup-directory-alist  '(("." . "~/.saves"))
+      savedir		      "~/.saves"
+      backup-directory-alist  '(("." . ,savedir))
       delete-old-versions     t
       kept-new-versions       6
       kept-old-versions       2
