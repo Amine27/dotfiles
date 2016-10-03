7192aae7ba63fc111ca41f08d17d928ddbcb44f1
Aziz Yemloul <azizyemloul@gmail.com>
2016-10-04 00:36:12 +0100

    Highlight codeblocks in orgmode

--- emacs-custom.el
+++ emacs-custom.el
@@ -41,4 +41,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(org-block ((t (:inherit shadow :background "white"))))
+ '(org-block-background ((t (:background "color-255"))))
+ '(org-block-begin-line ((t (:inherit org-meta-line :background "color-46"))) t)
+ '(org-block-end-line ((t (:inherit org-meta-line :background "color-46"))) t))
