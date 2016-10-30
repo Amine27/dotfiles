0a0fe7ebf01019689f3c753c627d5b30484f553a
Aziz Yemloul <azizyemloul@gmail.com>
2016-10-30 22:22:51 +0100

    disable sourceblock highlight in orgmode

--- emacs-custom.el
+++ emacs-custom.el
@@ -41,7 +41,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-block ((t (:inherit shadow :background "white"))))
+ '(hl-line ((t (:background "brightyellow"))))
+ '(org-block ((t (:inherit shadow))))
  '(org-block-background ((t (:background "color-255"))))
- '(org-block-begin-line ((t (:inherit org-meta-line :background "color-46"))) t)
- '(org-block-end-line ((t (:inherit org-meta-line :background "color-46"))) t))
+ '(org-block-begin-line ((t (:inherit org-meta-line))))
+ '(org-block-end-line ((t (:inherit org-meta-line)))))
