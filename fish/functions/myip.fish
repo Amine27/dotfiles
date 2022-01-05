0e3175f2d333259c616f565c6ec96e5f83a02f3e
Amine Roukh <amineroukh@gmail.com>
2022-01-05 16:32:29 +0100

    Fix myip func

--- fish/functions/myip.fish
+++ fish/functions/myip.fish
@@ -1,4 +1,4 @@
 function myip -d 'bash alias myip import'
-    curl -w '%{stdout}' ifconfig.me
-    # dig +short myip.opendns.com @resolver1.opendns.com $argv
+    # curl -w '%{stdout}' ifconfig.me
+    dig +short myip.opendns.com @resolver4.opendns.com $argv
 end
